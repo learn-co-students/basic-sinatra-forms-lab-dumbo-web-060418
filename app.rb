b94077441ed45cd12d6399132a4c72704283a4fb
robertexe <robtabares92@gmail.com>
2018-06-27 20:36:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
-require 'sinatra/base'
-
+require 'pry'
 class App < Sinatra::Base
 
+  get('/newteam'){
+		## created routing for the newteam
+		## it uses get method to read the form
+		erb :'newteam'
+	}
 
-end
+  post('/team'){
+		##created routing to /Team
+		## uses post method to create
+		erb :'team'
+	}
+
+
+  end
